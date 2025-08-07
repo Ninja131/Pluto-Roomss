543850ee8c278a21d8a4f7825905abd5c2fd53f0
Neeraj Choudhary <neerajneeraj1541@gmail.com>
2025-08-07 11:33:48 +0530

    aede
    nothing

---
+++ plutorooms/app/page.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl bg-blue-100 font-workSans">This uses Work Sans</h1>
+      <h1 className="display-xl bg-blue-100 font-workSans">This uses Monsieur</h1>
+
+       <h1 className="h1 bg-blue-100 font-workSans">This uses Monsieur</h1>
+
+        <h1 className="h2 bg-blue-100 font-workSans">This uses Monsieur</h1>
+
+         <h1 className="h3 bg-blue-100 font-workSans">This uses Monsieur</h1>
+         <h1 className="h4 bg-blue-100 font-workSans">This uses Monsieur</h1>
+
+         <h1 className="title bg-blue-100 font-workSans">This uses Monsieur</h1>
+
+         <h1 className="body-lg bg-blue-100 font-workSans">This uses Monsieur</h1>
+
+         <h1 className="body-sm bg-blue-100 font-workSans">This uses Monsieur</h1>
+
+         <h1 className="body-xs bg-blue-100 font-workSans">This uses Monsieur</h1>
+
+         <h1 className="label-sm bg-blue-100 font-workSans">This uses Monsieur</h1>
+
+         <h1 className="button bg-blue-100 font-workSans">This uses Monsieur</h1>
+
+        <Button variant="outline">Button</Button>
+    </>
+  );
+}
